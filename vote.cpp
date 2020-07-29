d3c5e13834b3d677d89117d64d8dd7811d540351
Maniroyal77 <67797945+Maniroyal77@users.noreply.github.com>
2020-07-29 20:09:54 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int age;
+  cout<<"enter the age of person:";
+  cin>>age;
+if(age>=18){
+  cout<<"Person is eligible to vote"<<age<<endl;
+}
+else {
+ cout<<"person is not eligible to vote"<<age<<endl;
+}
+    
+   
+    return 0;
+}
